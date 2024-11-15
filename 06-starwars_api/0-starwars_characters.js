99c0d88a0adead14e36a50e9d5b8cc93dcf5db82
Mershark Oheneba Takyi <113654948+mershark@users.noreply.github.com>
2024-11-14 21:43:04 -0400

    Update 0-starwars_characters.js

--- 06-starwars_api/0-starwars_characters.js
+++ 06-starwars_api/0-starwars_characters.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
 
 const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
-  if (err) throw err;
-  const actors = JSON.parse(body).characters;
-  exactOrder(actors, 0);
-});
-const exactOrder = (actors, x) => {
-  if (x === actors.length) return;
-  request(actors[x], function (err, res, body) {
-    if (err) throw err;
-    console.log(JSON.parse(body).name);
-    exactOrder(actors, x + 1);
+request(url, (err, res, body) => {
+  if (err) return console.error(err);
+
+  const characters = JSON.parse(body).characters;
+  characters.forEach(characterUrl => {
+    request(characterUrl, (err, res, body) => {
+      if (err) return console.error(err);
+      console.log(JSON.parse(body).name);
+    });
   });
-};
+});
